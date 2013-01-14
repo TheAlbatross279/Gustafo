0b67d4148dfa17a056705d3cab4622ba3e3bc1ca
Andrew Guenther <guenther.andrew.j@gmail.com>
2013-01-14 15:18:44 -0800

    Some rearranging and junk removal

--- gustafo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-
-python gustafo.py irc.freenode.net \#csc580 Gustafo-bot
-
--- test.py
+++
@@ -1,6 +0,0 @@
-from state import State, StateCollection
-from states.wikistate import WikiState
-
-bot = StateCollection([WikiState])
-
-print bot.query("Chris", "Tell me about John Adams")
