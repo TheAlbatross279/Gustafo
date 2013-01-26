bfed19f60dccc30873f1acb01322916b1bcc90f1
Andrew Guenther <guenther.andrew.j@gmail.com>
2013-01-26 15:25:21 -0800

    Fixed the state init file

--- bot/state/__init__.py
+++ bot/state/__init__.py
@@ -1,3 +1,13 @@
 """
 Gustafo is s knowledge based system that uses a state machine to transiition from one mode of chat to another. This package contains the states which Gustafo can enter.
 """
+
+import os
+import glob
+
+import os
+for module in os.listdir(os.path.dirname(__file__)):
+    if module == '__init__.py' or module[-3:] != '.py':
+        continue
+    __import__(module[:-3], locals(), globals())
+del module
