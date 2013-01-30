824ab0bbb900e6d0ebe58663f80819648e9136e1
Luke Larson <lplarson@calpoly.edu>
2013-01-29 21:57:33 -0800

    Fixed a few errors in tables.sql

--- db/tables.sql
+++ db/tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE question (
     creator VARCHAR(255) REFERENCES user,
     editor VARCHAR(255) REFERENCES user,
     title VARCHAR(255),
-    text VARCHAR(65535)
+    text VARCHAR(65535),
     rating INTEGER,
     favorited INTEGER,
     created DATE,
@@ -23,6 +23,7 @@ CREATE TABLE qcomment (
     qcid INTEGER PRIMARY KEY,
     qid INTEGER REFERENCES question,
     user VARCHAR(255) REFERENCES user,
+    text VARCHAR(65535),
     rating INTEGER,
     time DATE
 );
@@ -31,12 +32,13 @@ CREATE TABLE acomment (
     acid INTEGER PRIMARY KEY,
     aid INTEGER REFERENCES answer,
     user VARCHAR(255) REFERENCES user,
+    text VARCHAR(65535),
     rating INTEGER,
     time DATE
 );
 
 CREATE TABLE user (
-    user VARCHAR(255) PRIMARY KEY
+    user VARCHAR(255) PRIMARY KEY,
     reputation INTEGER,
     gold_badge INTEGER,
     silver_badge INTEGER,
@@ -50,6 +52,6 @@ CREATE TABLE tag (
 -- Many to many relationship
 -- Tags can references many questions, and questions can reference many tags
 CREATE TABLE tag_question (
-    tag REFERENCES tag,
-    qid REFERENCES question
+    tag VARCHAR(32) REFERENCES tag,
+    qid INTEGER REFERENCES question
 );
