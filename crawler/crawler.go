aafa45fceef01d7dba3d2055876a4253036c152e
Ross Light <ross@zombiezen.com>
2013-01-29 16:11:01 -0800

    Change rate-limiter to client level (allows path-based backoff)

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -5,25 +5,21 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
-	"time"
 )
 
 var (
 	Site = stackexchange.StackOverflow
-	Client *stackexchange.Client
+	Client StackExchangeClient
 )
 
 func main() {
 	flag.StringVar(&Site, "site", Site, "the site name to scrape from")
 	flag.Parse()
 
-	transport := NewThrottledRoundTripper(http.DefaultTransport)
-	defer transport.Close()
-	Client = &stackexchange.Client{
-		Client: &http.Client{
-			Transport: transport,
-		},
+	{
+		rlc := NewRateLimitClient(stackexchange.DefaultClient)
+		defer rlc.Close()
+		Client = rlc
 	}
 
 	for {
@@ -45,80 +41,4 @@ func main() {
 		}
 		fmt.Println()
 	}
-}
-
-// ThrottledRoundTripper rate-limits requests sent to an underlying http.RoundTripper.
-type ThrottledRoundTripper struct {
-	http.RoundTripper
-	c    chan roundTripRequest
-	quit chan struct{}
-}
-
-func NewThrottledRoundTripper(rt http.RoundTripper) *ThrottledRoundTripper {
-	t := &ThrottledRoundTripper{
-		RoundTripper: rt,
-		c:            make(chan roundTripRequest),
-		quit:         make(chan struct{}),
-	}
-	go t.run()
-	return t
-}
-
-type roundTripRequest struct {
-	r *http.Request
-	c chan roundTripResponse
-}
-
-type roundTripResponse struct {
-	resp *http.Response
-	err  error
-}
-
-func (rt *ThrottledRoundTripper) run() {
-	const (
-		perDay    = 10000
-		perSecond = 30
-
-		tickDuration = time.Second / perSecond
-	)
-
-	remaining := perDay
-	day := time.Tick(24 * time.Hour)
-	ticker := time.NewTicker(tickDuration)
-	tickChan := ticker.C
-	for {
-		select {
-		case <-tickChan:
-			req := <-rt.c
-			resp, err := rt.RoundTripper.RoundTrip(req.r)
-			req.c <- roundTripResponse{resp, err}
-
-			remaining--
-			if remaining <= 0 {
-				ticker.Stop()
-				ticker = nil
-				tickChan = nil
-			}
-		case <-day:
-			remaining += perDay
-			if ticker == nil {
-				ticker = time.NewTicker(tickDuration)
-				tickChan = ticker.C
-			}
-		case <-rt.quit:
-			return
-		}
-	}
-}
-
-func (rt *ThrottledRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	c := make(chan roundTripResponse)
-	rt.c <- roundTripRequest{req, c}
-	resp := <-c
-	return resp.resp, resp.err
-}
-
-func (rt *ThrottledRoundTripper) Close() error {
-	close(rt.c)
-	return nil
 }
--- crawler/ratelimit.go
+++ crawler/ratelimit.go
@@ -2,6 +2,18 @@ package main
 
 import (
 	"bitbucket.org/zombiezen/stackexchange"
+	"log"
+	"sync"
+	"time"
+)
+
+const (
+	apiPerSecond = 30
+	apiPerDay    = 10000
+
+	dayDuration = 24 * time.Hour
+
+	secondsPerApi = time.Second / apiPerSecond
 )
 
 type StackExchangeClient interface {
@@ -9,30 +21,114 @@ type StackExchangeClient interface {
 }
 
 // RateLimitClient rate-limits requests sent to an underlying StackExchangeClient.
+// A RateLimitClient is safe to use from multiple goroutines.
 type RateLimitClient struct {
 	client StackExchangeClient
-	c      chan struct{}
+	c      chan *apiRequest
+	locker keyLocker
 }
 
 func NewRateLimitClient(client StackExchangeClient) *RateLimitClient {
 	rlc := &RateLimitClient{
 		client: client,
-		c:      make(chan struct{}),
+		c:      make(chan *apiRequest),
 	}
 	go rlc.run()
 	return rlc
 }
 
 func (rlc *RateLimitClient) run() {
-	// TODO
+	day := time.NewTicker(dayDuration)
+	defer day.Stop()
+
+	quotaRemaining := apiPerDay
+	var lastRequest time.Time
+	for {
+		for quotaRemaining > 0 {
+			select {
+			case req, ok := <-rlc.c:
+				if !ok {
+					return
+				}
+				now := time.Now()
+				if elapsed := now.Sub(lastRequest); elapsed < secondsPerApi {
+					time.Sleep(secondsPerApi - elapsed)
+					now = time.Now()
+				}
+				lastRequest = now
+				go rlc.handle(req)
+			case <-day.C:
+				quotaRemaining += apiPerDay
+			}
+		}
+
+		<-day.C
+		quotaRemaining += apiPerDay
+	}
+}
+
+func (rlc *RateLimitClient) handle(req *apiRequest) {
+	lock := rlc.locker.Get(req.path)
+	lock.Lock()
+	defer lock.Unlock()
+	log.Println("API:", req.path)
+	wrapper, err := rlc.client.Do(req.path, req.v, req.params)
+	log.Printf("%+v", wrapper)
+	req.c <- &apiResponse{wrap: wrapper, err: err}
+	if wrapper.Backoff > 0 {
+		dur := time.Duration(wrapper.Backoff) * time.Second
+		log.Printf("  backoff: %s (t=%v)", req.path, dur)
+		time.Sleep(dur)
+	}
 }
 
 func (rlc *RateLimitClient) Do(path string, v interface{}, params stackexchange.Params) (*stackexchange.Wrapper, error) {
-	// TODO
-	return nil, nil
+	c := make(chan *apiResponse)
+	rlc.c <- &apiRequest{
+		path:   path,
+		v:      v,
+		params: params,
+		c:      c,
+	}
+	resp := <-c
+	return resp.wrap, resp.err
 }
 
 func (rlc *RateLimitClient) Close() error {
 	close(rlc.c)
 	return nil
+}
+
+type apiRequest struct {
+	path   string
+	v      interface{}
+	params stackexchange.Params
+	c      chan *apiResponse
+}
+
+type apiResponse struct {
+	wrap *stackexchange.Wrapper
+	err  error
+}
+
+// keyLocker is a key-value lock store.  The zero value is an empty mapping.
+type keyLocker struct {
+	mutex sync.Mutex
+	m     map[string]*sync.Mutex
+}
+
+// Get returns the lock for the key, creating a lock if necessary.
+func (kl *keyLocker) Get(key string) sync.Locker {
+	kl.mutex.Lock()
+	defer kl.mutex.Unlock()
+
+	if kl.m == nil {
+		kl.m = make(map[string]*sync.Mutex)
+	}
+	lock := kl.m[key]
+	if lock == nil {
+		lock = new(sync.Mutex)
+		kl.m[key] = lock
+	}
+	return lock
 }
