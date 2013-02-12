d3c2587e412c36c2b12a15f7d0eb9ced20f65c44
Mike Lady <michaellady@gmail.com>
2013-02-12 08:31:11 -0800

    added pairings parsing

--- db/seed.py
+++ db/seed.py
@@ -8,8 +8,17 @@ def insertPhrases (phrases_csv):
             for (row) in enumerate(phrasereader):
                 output.write('\n('+str(row[1])[1:-1]+'), ')
                 output.write('--'+str(row[0]+1))
+ 
+def insertPairs (pairs_csv):
+    with open(pairs_csv[:-3]+'sql','w') as output:
+        output.write('INSERT INTO DATA_CONVO_PAIRS (utterance_id, response_id) VALUES')
+        with open(pairs_csv, 'Urb') as f:
+            phrasereader = csv.reader(f, delimiter=',')
+            for (row) in enumerate(phrasereader):
+                output.write('\n('+str(row[1])[1:-1]+'), ')
         
 
 if __name__ == "__main__":
     import sys
     insertPhrases(sys.argv[1])
+    insertPairs(sys.argv[2])
