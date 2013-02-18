c924dbbd17beedc4a63e2c221c9b61b0e70f88cf
Daniel <gilliland.daniel@gmail.com>
2013-02-18 00:00:05 +0000

    Basic inference has been implemented. I'll explain tonight at the meeting, or you could look at the code

--- processor/inference/hInferenceEngine.py
+++ processor/inference/hInferenceEngine.py
@@ -1,36 +1,49 @@
+from db.psql import HelpConnection
 """
 
 """
 
 class HInferenceEngine():
    def __init__(self):
-      self.dbConnection = None
+      self.dbConnection = HelpConnection()
 
    """
    takes list of strings as input, returns string
    """
    def infer(self, msg):
-      msgID = self.findID(msg)
-      response = ""
-      
-      #if msg does not exist in db
-      if msgID is None: 
-          response = self.genResponse(msg)
-      #if exists, find response
-      else:
-          response = self.lookupResponse(msg)
+
+      response = self.findAnswer(msg)
 
       return response
 
-   #find ID of phrase in db
-   def findID(msg):
-       pass
+   #Finds the most likely answer in a very basic way
+   def findAnswer(self, msg):
+      qidDict = {}
+      
+      #Iterate over all keywords
+      for keyword in msg:
+         #Save all qids associated with the specified keyword
+         qids = self.dbConnection.query("SELECT question.qid FROM question WHERE question.title SIMILAR TO \'%" + keyword + "%\'")
+         for qid in qids:
+            try:
+               qidDict[qid]
+            except:
+               qidDict[qid] = 0
+            #For every qid found, increment its value in a dictionary
+            qidDict[qid] = qidDict[qid] + 1
 
-   #finds response based on ID
-   def lookupResponse(msg, msgID):
-       pass
+      bestqid = 0
+      high = 0
+      #Iterate over the dictionary results, the highest valued qid is most likely
+      for val in qidDict:
+         if qidDict[val] > high:
+            high = qidDict[val]
+            bestqid = val[0]
 
-   #generates a response based on msg
-   def genResponse(msg):
-       pass
-   
+      #Find the answer associated with the best qid
+      answers = self.dbConnection.query("SELECT answer.text FROM answer WHERE answer.qid = \'" + str(bestqid) + "\'")
+
+      try:
+         return answers[0]
+      except:
+         return "I could not find an answer"
