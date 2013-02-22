c786c60fa811a7ca781e7cdba73a9824372fffa2
Daniel <gilliland.daniel@gmail.com>
2013-02-22 01:25:22 +0000

    Mongo to Postgres script finished. Use populateQuestionDatabase to update with new information closes #35

--- db/populateQuestionDatabase.py
+++ db/populateQuestionDatabase.py
@@ -4,6 +4,10 @@ import psycopg2
 import datetime
 from HTMLParser import HTMLParser
 
+#Set up the postgres database connection
+pconn = psycopg2.connect(database='mydb', user='postgres', password = 'password')
+pgCur = pconn.cursor()
+
 class MLStripper(HTMLParser):
    def __init__(self):
       self.reset()
@@ -24,9 +28,67 @@ def formatString(text):
 def formatDate(dateNum):
    return datetime.datetime.fromtimestamp(dateNum)
 
-#Setting up the postgres connection
-pconn = psycopg2.connect(database='mydb', user='postgres', password = 'password')
-pgCur = pconn.cursor()
+def saveUser(uname, uRep):
+   #Query the DB to see if the user already exists
+   pgCur.execute("SELECT * FROM so_user WHERE so_user = %s", [uname])
+   result = pgCur.fetchone()
+
+   #If the user does not exist, save it
+   if(result == None):
+      pgCur.execute("INSERT INTO so_user (so_user, reputation) VALUES (%s, %s)", (uname, uRep))
+
+
+#Saves all comments for the specified answer
+def saveAnswerComments(comments, aid):
+   for comment in comments:
+      
+      curU = comment['owner']
+      uname = curU['display_name']
+      uRep = 0
+
+      saveUser(uname, uRep)
+
+      cId = comment['comment_id']
+      cBody = formatString(comment['body'])
+      rating = comment['score']
+      date = formatDate(comment['creation_date'])
+
+      pgCur.execute("INSERT INTO acomment (acid, aid, so_user, text, rating, time) VALUES (%s, %s, %s, %s, %s, %s)", (cId, aid, uname, cBody, rating, date))
+
+
+
+#Saves all comments for the specified question
+def saveQuestionComments(comments, qid):
+   for comment in comments:
+      
+      curU = comment['owner']
+      uname = curU['display_name']
+      uRep = 0
+
+      saveUser(uname, uRep)
+
+      cId = comment['comment_id']
+      cBody = formatString(comment['body'])
+      rating = comment['score']
+      date = formatDate(comment['creation_date'])
+
+      pgCur.execute("INSERT INTO qcomment (qcid, qid, so_user, text, rating, time) VALUES (%s, %s, %s, %s, %s, %s)", (cId, qid, uname, cBody, rating, date))
+
+#Saves all tags associated with the quesiton in the database
+def saveTags(tags, qid):
+   for tag in tags:
+
+      #See if the tag exists
+      pgCur.execute("SELECT * FROM tag WHERE tag = %s", [tag])
+      result = pgCur.fetchone()
+
+      if(result == None):
+         pgCur.execute("INSERT INTO tag (tag) VALUES (%s)", [tag])
+
+      #Add the relationship
+      pgCur.execute("INSERT INTO tag_question (tag, qid) VALUES (%s, %s)", (tag, qid))
+
+
 
 #Setting up the mongo connection
 connection = MongoClient()
@@ -59,13 +121,8 @@ for i in range(qNum):
    uname = curU['display_name']
    uRep = 0#curU['reputation']
 
-   #Query the DB to see if the user already exists
-   pgCur.execute("SELECT * FROM so_user WHERE so_user = %s", [uname])
-   result = pgCur.fetchone()
-
-   #If the user does not exist, save it
-   if(result == None):
-      pgCur.execute("INSERT INTO so_user (so_user, reputation) VALUES (%s, %s)", (uname, uRep))
+   #Make sure the user exists
+   saveUser(uname, uRep)
 
    #Pull all attributes off the question
    title = formatString(curQ['title'])
@@ -80,6 +137,15 @@ for i in range(qNum):
    #Save the question in the postgresDB
    pgCur.execute("INSERT INTO question (qid, creator, editor, title, text, rating, favorited, created, edited) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (qid, creator, editor, title, body, rating, favorited, created, edited))
 
+   #Iterate over all question comments and persist them
+   try:
+      comments = curQ['comments']
+      saveQuestionComments(comments, qid)
+   except:
+      pass
+
+   tags = curQ['tags']
+   saveTags(tags, qid)
 
    #Find all the answers associated with this question
    allAnswers = answers.find({"question_id":qid})
@@ -99,19 +165,20 @@ for i in range(qNum):
       user = curA['owner']['display_name']
       body = formatString(curA['body'])
       rating = curA['up_vote_count']
+      isAccepted = curA['is_accepted']
       time = formatDate(curA['creation_date'])
 
-      #Query the DB to see if the user already exists
-      pgCur.execute("SELECT * FROM so_user WHERE so_user = %s", [uname])
-      result = pgCur.fetchone()
-
-      #If the user does not exist, save it
-      if(result == None):
-         pgCur.execute("INSERT INTO so_user (so_user, reputation) VALUES (%s, %s)", (uname, uRep))
+      saveUser(uname, uRep)
 
       #Save the answer in the postgresDB
-      pgCur.execute("INSERT INTO answer (aid, qid, so_user, text, rating, time) VALUES (%s, %s, %s, %s, %s, %s)", (aid, qid, user, body, rating, time))
+      pgCur.execute("INSERT INTO answer (aid, qid, so_user, text, rating, isAccepted, time) VALUES (%s, %s, %s, %s, %s, %s, %s)", (aid, qid, user, body, rating, isAccepted, time))
 
+      try:
+         comments = curA['comments']
+         saveAnswerComments(comments, aid)
+      except:
+         pass
+      
 
    print str(i) + " Questions saved"
 
