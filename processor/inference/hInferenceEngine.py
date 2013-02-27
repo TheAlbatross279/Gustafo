b1faaab6d5f0fb42b05c5b7631554cb4f45c860a
Daniel <gilliland.daniel@gmail.com>
2013-02-27 06:30:48 +0000

    Added more wonderful euclidian distance on question relevance

--- processor/inference/hInferenceEngine.py
+++ processor/inference/hInferenceEngine.py
@@ -1,4 +1,5 @@
 from db.psql import HelpConnection
+from processor.nlp.filter.normalTokFilter import NormalTokFilter
 """
 
 """
@@ -6,24 +7,83 @@ from db.psql import HelpConnection
 class HInferenceEngine():
    def __init__(self):
       self.dbConnection = HelpConnection()
+      self.filt = NormalTokFilter()
+      self.title_weight = 0.1
+      self.tag_weight = 0.1
+      self.rating_weight = 0.1
+      self.body_weight = 0.1
 
    """
    takes list of strings as input, returns string
    """
    def infer(self, msg):
 
-      response = self.findAnswer(msg)
+      qid = self.findBestQuestion(msg)
+
+      response = self.findAnswer(qid)
 
       return response
 
+   
+   def findBestQuestion(self, msg):
+      qDict = {}
+
+      #Iterate over all user entered words
+      for keyword in msg:
+         #This will be replaced with the index query
+         qs = self.dbConnection.query(("SELECT * FROM question WHERE question.title ILIKE %s", ['%'+keyword+'%']))
+
+         for q in qs:
+            qDict[q] = 0
+
+      for q in qDict.keys():
+         (qid, _, _, title, body, rating, num_views, favorited, _, _) = q
+
+         rank = self.rankQuestion(q, msg)
+
+         qDict[q] = (rank, qid)
+       
+      best = max(qDict.values(), key=lambda x: x[0])
+
+      return best[1]
+
+   def rankQuestion(self, question, msg):
+      (_, _, _, title, body, rating, num_views, favorited, _, _) = question
+      
+      title = self.filt.filter(title)
+      
+      titleScore = self.title_weight * self.calcDist(title, msg)
+
+      rank = titleScore
+
+      return rank
+
+   def calcDist(self, title, message):
+
+      title_features = set([])
+      title_features = title_features.union(title).union(message)
+
+      titleVector = [0]*len(title_features)
+      messageVector = [0]*len(title_features)
+
+      for i, word in enumerate(title_features):
+         if word in title:
+            titleVector[i] = 1
+         if word in message:
+            messageVector[i] = 1
+
+      return len(title_features) - sum([(x - y)**2 for x, y in zip(titleVector, messageVector)])
+
+
    #Finds the most likely answer in a very basic way
-   def findAnswer(self, msg):
+   def findAnswer(self, qid):
+      '''
       qidDict = {}
       
       #Iterate over all keywords
       for keyword in msg:
          #Save all qids associated with the specified keyword
-         qids = self.dbConnection.query(("SELECT question.qid FROM question WHERE question.title SIMILAR TO %s", ['%'+keyword+'%']))
+         qids = self.dbConnection.query(("SELECT question.qid FROM question WHERE question.title ILIKE %s", ['%'+keyword+'%']))
          for qid in qids:
             try:
                qidDict[qid]
@@ -39,9 +99,9 @@ class HInferenceEngine():
          if qidDict[val] > high:
             high = qidDict[val]
             bestqid = val[0]
-
+      '''
       #Find the answer associated with the best qid
-      answers = self.dbConnection.query(("SELECT answer.text FROM answer WHERE answer.qid = %s", [str(bestqid)]))
+      answers = self.dbConnection.query(("SELECT answer.text FROM answer WHERE answer.qid = %s", [str(qid)]))
 
       try:
          return answers[0]
