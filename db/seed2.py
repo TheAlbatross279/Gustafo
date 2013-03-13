6aedee451c74e9ebe9802e98a47cd9f9888571c0
Luke Larson <luke@domU-12-31-39-06-AD-E2.compute-1.internal>
2013-03-13 01:48:30 +0000

    Added seed2

---
+++ db/seed2.py
@@ -0,0 +1,19 @@
+import csv
+
+def insertPhrases (phrases_csv):
+    with open(phrases_csv[:-3] + 'sql','w') as output:
+        with open(phrases_csv, 'Urb') as f:
+            phrasereader = csv.reader(f, delimiter=',')
+            for row in phrasereader:
+                output.write('INSERT INTO DATA_PHRASES (orig_utterance, sani_utterance) VALUES (' + str(row)[1:-1] + ');\n')
+ 
+def insertPairs (pairs_csv):
+    with open(pairs_csv[:-3]+'sql','w') as output:
+        with open(pairs_csv, 'Urb') as f:
+            phrasereader = csv.reader(f, delimiter=',')
+            for row in phrasereader:
+                output.write('INSERT INTO DATA_CONVO_PAIRS (utterance_id, response_id) VALUES (' + str(row)[1:-1] + ');\n')
+
+if __name__ == "__main__":
+    insertPhrases('insert_seeds.csv')
+    insertPairs('insert_pairings.csv')
